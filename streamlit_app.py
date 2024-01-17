15683495b50c41a3b84ded9a0c5c97549d261b31
snehaTBNOV <144178731+snehaTBNOV@users.noreply.github.com>
2024-01-17 13:47:54 +0000

    Update streamlit_app.py
    Added detailed description

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('my parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3  & Blueberry Oatmeal')
+streamlit.text('Kale Spinach & Rocket smoothie')
+streamlit.text('Hard Boiled egg')
