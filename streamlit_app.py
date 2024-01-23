3a8876c799d5b9344cd4d6dd2d018d9d6675c5d1
snehaTBNOV <144178731+snehaTBNOV@users.noreply.github.com>
2024-01-23 11:32:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 #streamlit.text("hello from sf:")
 streamlit.header("fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+streamlit.write(my_data_rows)
 
 
